d5e6eac85b7d829ac61ff34f92b4c81a3c4c8a7d
Diego Rojas <diegorojas431@gmail.com>
2022-01-26 19:52:03 -0500

    card

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 import '@styles/style.css';
-import pokemon from '@images/pokemon.png';
+import App from './App';
 
 const $root = document.querySelector('#root')
 
-ReactDom.render(<>
-  <h1 className='hello'>Hello React!</h1>
-  <img src={pokemon} alt="pokemon"/>
-</>, $root)
+ReactDom.render(<App />, $root)
